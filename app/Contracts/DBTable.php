ecd051fc31400bf504b73c89c2b7d3b34b6d3cd7
khanhpham <khanhpham.ask@gmail.com>
2017-07-24 23:54:52 +0700

    Add database migration

---
+++ app/Contracts/DBTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Contracts;
+
+/**
+ * Defined tables' name.
+ */
+class DBTable
+{
+    const USER = 'users';
+    const UPLOADS = 'uploads';
+    const PASSWORD_RESET = 'password_resets';
+}
